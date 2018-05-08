f94f0cbc2721d686143ce425b87061a6d371102e
Danil <danil.present@mail.ru>
2018-05-08 19:33:42 +0800

    Add print with delay and add clear screen function

--- ezprint.py
+++ ezprint.py
@@ -1,3 +1,20 @@
 
+import time
+import sys
+import os
+
+
 def p(value = ''):
 	print(value)
+
+
+def pvd(s = ''):
+	strtoprint = ''
+	for i in s:
+		strtoprint = strtoprint + str(i)
+		sys.stdout.write('\r\r' + (strtoprint) + '')
+		time.sleep(0.01)
+
+
+def cls():
+	os.system('cls')
